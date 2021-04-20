fb37b366df6fec9d28f43ff5b0ec5961f85cb1ea
hyunho.lee <mizzking75@gmail.com>
2021-04-20 23:36:51 +0900

    Fix ViewController

---
+++ PracticeModalPresent/PracticeModalPresent/MyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyViewController.swift
+//  PracticeModalPresent
+//
+//  Created by 이현호 on 2021/04/20.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
--- PracticeModalPresent/PracticeModalPresent/ViewController.swift
+++ PracticeModalPresent/PracticeModalPresent/ViewController.swift
@@ -15,8 +15,18 @@ class ViewController: UIViewController {
 
     @IBAction func clickPresentButton(_ sender: Any) {
         guard let vcName = self.storyboard?.instantiateViewController(withIdentifier: "NewViewController") else { return }
+        vcName.modalPresentationStyle = .fullScreen
         self.present(vcName, animated: true, completion: nil)
     }
     
+    @IBAction func clickPresentByCodeButton(_ sender: Any) {
+        let myViewController = MyViewController()
+        self.present(myViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func navigationPushButton(_ sender: Any) {
+        guard let pushVC = self.storyboard?.instantiateViewController(withIdentifier: "OrangeViewController") else { return }
+        self.navigationController?.pushViewController(pushVC, animated: true)
+    }
 }
 
